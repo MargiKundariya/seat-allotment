52b69d1dc7aab11586a0b1e14aca92d7b38a33e9
Margikundariya <margikundariya@gmail.com>
2026-03-06 09:09:20 +0530

    update server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 mongoose.connect("mongodb+srv://projecttiral7_db_user:seatallotment_0102@cluster0.ro8zthy.mongodb.net/seatAllotmentDB")
   .then(() => console.log("MongoDB Altas connected"))
   .catch((err) => console.log(err));
@@ -15,6 +15,7 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/cutoff", require("./routes/cutoffRoutes"));
 app.use("/api/searchcutoff", require("./routes/searchRoutes"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
