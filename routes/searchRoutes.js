270c68d299f98bbb782d624994880fcbd27e92a9
MargiKundariya <144773551+MargiKundariya@users.noreply.github.com>
2026-03-13 10:38:20 +0530

    Delete routes directory

--- routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { searchByRank } = require("../controller/searchController");
-
-router.post("/search", searchByRank);
-
-module.exports = router;
