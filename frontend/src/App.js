0ea828cbd301018c90c25893999752f494467110
MargiKundariya <144773551+MargiKundariya@users.noreply.github.com>
2026-03-13 12:32:52 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Registration from "./components/Registration";
 import AddCollegeCutoff from "./components/AddCollegeCutoff";
