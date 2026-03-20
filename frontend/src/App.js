d577ab8f0298da5f47e6639a153663abc43dc486
MargiKundariya <144773551+MargiKundariya@users.noreply.github.com>
2026-03-20 20:23:03 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,22 @@ import Registration from "./components/Registration";
 import AddCollegeCutoff from "./components/AddCollegeCutoff";
 import CollegeCutoffSearch from "./components/Collegecutoffsearch";
 import ForgotPassword from "./components/forgetpassword";
+
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Default route */}
         <Route path="/" element={<Login />} />
-        <Route path="/registration" element={<Registration />} />
+
+        {/* Auth routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+
+        {/* Other pages */}
         <Route path="/addcollegecutoff" element={<AddCollegeCutoff />} />
         <Route path="/collegecutoffsearch" element={<CollegeCutoffSearch />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
-
       </Routes>
     </Router>
   );
